c18bd84d3fe078f515c08cdd0531e1fc9d1d866b
Wandy <hanyudha@gmail.com>
2023-02-02 15:11:40 +0700

    initial hr-sample
    Signed-off-by: Wandy <hanyudha@gmail.com>

---
+++ hr-sample/src/main/java/com/example/hrsample/HrSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.hrsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrSampleApplication.class, args);
+    }
+
+}
---
+++ hr-sample/src/test/java/com/example/hrsample/HrSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
